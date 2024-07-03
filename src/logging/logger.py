2a4f88a1ff0776ea1e5ee171fdfecc9aa751b3b2
Ramakrishna Reddy Palle <137872198+RamakrishnaReddyPalle@users.noreply.github.com>
2024-07-03 11:31:19 +0530

    logger added

---
+++ src/logging/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    file_handler = logging.FileHandler('logs/prediction_pipeline.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
